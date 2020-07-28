c0f4c564e4033c6aaf77f3e90a8e7aa20a1dd89e
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2020-07-28 21:16:51 +0200

    automatically clone needed polybar plugins

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,6 +3,17 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+BASE_DIR="~/.config/polybar/"
+PLUGINS=("marioortizmanero/polybar-pulseaudio-control" "Jvanrhijn/polybar-spotify")
+
+for plugin in "${PLUGINS[@]}"
+do
+	IFS='/' read -ra psplit <<< "$plugin"
+	if [ ! -d "$BASE_DIR${psplit[1]}" ]; then
+		git clone https://github.com/$plugin.git
+	fi
+done
+
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 		MONITOR=$m polybar example &
