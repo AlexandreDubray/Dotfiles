ff4ca9b44a19104c38e4673f99fcc05addea4e06
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2020-07-30 09:02:12 +0200

    fix plugin installations for polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,14 +3,14 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-BASE_DIR="~/.config/polybar/"
+BASE_DIR="$HOME/.config/polybar/"
 PLUGINS=("marioortizmanero/polybar-pulseaudio-control" "Jvanrhijn/polybar-spotify")
 
 for plugin in "${PLUGINS[@]}"
 do
 	IFS='/' read -ra psplit <<< "$plugin"
 	if [ ! -d "$BASE_DIR${psplit[1]}" ]; then
-		git clone https://github.com/$plugin.git
+		git clone https://github.com/$plugin.git "$BASE_DIR${psplit[1]}"
 	fi
 done
 
